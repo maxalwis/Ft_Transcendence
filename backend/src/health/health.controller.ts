817208a903ce317725787c7cb3437b99878753ba
Simon Leroy <sim.leroy@hotmail.fr>
2026-08-01 20:23:03 +0200

    docs: add comments in PrismaService and HealthController

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { PrismaService } from '../prisma/prisma.service';
 
+// Par la suite, ajouter d'autres fonctions au tableau check([])
+// Permet de voir dans /health que tout tourne
 @Controller('health')
 export class HealthController {
   constructor(
@@ -11,7 +13,7 @@ export class HealthController {
 
   @Get()
   @HealthCheck()
-  check() {
+  readiness() {
     return this.health.check([
       async () => {
         await this.prisma.$queryRaw`SELECT 1`;
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -2,8 +2,11 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
+// adapter Postgres obligatoire avec Prisma 7
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
+// Connexion/déconnexion du client Prisma, gérées automatiquement par NestJS
+// via les hooks de cycle de vie onModuleInit() et onModuleDestroy().
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
