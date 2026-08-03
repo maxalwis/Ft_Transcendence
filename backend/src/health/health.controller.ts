d9b2626d23a7cf830e7ff96bf6fead2605f127e7
Simon Leroy <sim.leroy@hotmail.fr>
2026-08-03 10:40:47 +0200

    refactor (health): separate database check + change to HealthIndicatorService
    -Replace deprecated healthCheckError by healthIndicatorService
    -Create checkDatabase method for more modularity
    -Add comments for documentation

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -1,24 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, HealthIndicatorService } from '@nestjs/terminus';
 import { PrismaService } from '../prisma/prisma.service';
 
-// Par la suite, ajouter d'autres fonctions au tableau check([])
 // Permet de voir dans /health que tout tourne
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
-    private prisma: PrismaService
+    private prisma: PrismaService,
+    private healthIndicatorService: HealthIndicatorService,
   ) {}
 
   @Get()
   @HealthCheck()
   readiness() {
     return this.health.check([
-      async () => {
-        await this.prisma.$queryRaw`SELECT 1`;
-        return { database: { status: 'up' } };
-      },
+      () => this.checkDatabase(),
+      // par la suite : () => this.checkRedis(), () => this.checkNginx(), etc.
     ]);
+  }
+
+  // Vérifie que la connexion à BDD Postgres fonctionne avec une query minimale
+  private async checkDatabase() {
+    const indicator = this.healthIndicatorService.check('database');
+
+    try {
+      await this.prisma.$queryRaw`SELECT 1`;
+      return indicator.up();
+    } catch (e) {
+      return indicator.down();
+    }
   }
 }
