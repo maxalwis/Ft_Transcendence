05f5bd7fd705be94923875a05176282c45e7d3ef
Flavien Lebrun <prolebrunflavien@gmail.com>
2026-08-10 16:08:39 +0200

    [BE/FE] Added console.log for GET requests

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Query, Get, Param } from '@nestjs/common';
+import { Controller, Query, Get, Param, Logger } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { MapQueryDto } from './dto/map-query.dto';
 
 @Controller('events')
 export class EventsController {
+  private readonly logger = new Logger(EventsController.name);
   constructor(private eventsService: EventsService) {}
 
   @Get('map')
   findForMap(@Query() query: MapQueryDto) {
+    this.logger.log(
+      `GET /events/map - BBox: [${query.bbox.minLon.toFixed(4)}, ${query.bbox.minLat.toFixed(4)}, ${query.bbox.maxLon.toFixed(4)}, ${query.bbox.maxLat.toFixed(4)}]`
+    );
     return this.eventsService.findForMap(query.bbox);
   }
 
