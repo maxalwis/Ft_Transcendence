16e785be0c68d59142307b3810c7b01d317d28e8
Flavien Lebrun <prolebrunflavien@gmail.com>
2026-08-11 14:49:26 +0200

    Removed incorrectly merged parts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,36 +1,19 @@
-import { Controller, Query, Get, Param, Logger } from '@nestjs/common';
-import { EventsService } from './events.service';
-import { MapQueryDto } from './dto/map-query.dto';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
-@Controller('events')
-export class EventsController {
-  private readonly logger = new Logger(EventsController.name);
-  constructor(private eventsService: EventsService) {}
-
-  @Get('map')
-  findForMap(@Query() query: MapQueryDto) {
-    this.logger.log(
-      `GET /events/map - BBox: [${query.bbox.minLon.toFixed(4)}, ${query.bbox.minLat.toFixed(4)}, ${query.bbox.maxLon.toFixed(4)}, ${query.bbox.maxLat.toFixed(4)}]`
-    );
-    return this.eventsService.findForMap(query.bbox);
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(id);
-  }
-}
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            whitelist: true,
-            enableDebugMessages: true,
-        }),
-    );
-    
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      enableDebugMessages: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
