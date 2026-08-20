aa72719a478cda6b5ad9b63fa503427be2b18820
maalwis <maalwis@f4r2s12.paris.42.school>
2026-08-20 16:00:46 +0200

    page de connexion cree, avec bouton home, google et 42

--- front/src/components/BottomBar/Auth/LoginPage.tsx
+++ front/src/components/BottomBar/Auth/LoginPage.tsx
@@ -13,11 +13,11 @@ export default function LoginPage()
 	};
 
 	const handleGoogleLogin = () => {
-		window.location.href = "URL_AUTORISATION_GOOGLE";
+		window.location.href = "https://accounts.google.com/o/oauth2/auth?client_id=513700796427-64gvloa5ugr6ugebfne104u5v44u1en7.apps.googleusercontent.com&redirect_uri=https://localhost:8443&response_type=code&scope=email%20profile";
 	};
 
 	const handle42Login = () => {
-		window.location.href = "URL_AUTORISATION_42";
+		window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-eafc486920117f3b35ec72b7d5397bd65f1a26b0a04ba905103413815298e2f7&redirect_uri=https%3A%2F%2Flocalhost%3A8443&response_type=code";
 	};
 
 	const navigate = useNavigate();
@@ -26,8 +26,8 @@ export default function LoginPage()
 	};
 
 	return (
-	<div className='min-h-screen bg-gray-100 rounded-sm border-2 flex flex-col justify-center items-center'>
-		<div className='border-2 p-10 rounded-sm border-orange-500'>
+	<div className='min-h-screen bg-gray-900 rounded-sm border-2 flex flex-col justify-center items-center'>
+		<div className='border-2 p-10 rounded-sm border-orange-500 bg-white'>
 			<h1 className='font-extrabold text-3xl'>Sign in to your account</h1>
 
 			<form
@@ -39,14 +39,14 @@ export default function LoginPage()
 					htmlFor="username"> Username </label>
 				<input
 					className='glassmorphism-popup pl-2 flex items-center jutify-center rounded-2xl border-2'
-					type="text"
+					type="username"
 					value={username}
 					onChange={(e) => setUsername(e.target.value)}
 					placeholder="Username"/>
+
 				<label
 					className='mb-1 mt-3'
 					htmlFor="password"> Password </label>
-
 				<input
 					className='glassmorphism-popup pl-2 flex items-center jutify-center rounded-lg border-2'
 					type="password"
@@ -54,20 +54,32 @@ export default function LoginPage()
 					onChange={(e) => setPassword(e.target.value)}
 					placeholder="Password"/>
 			<button
-				className="mt-4 flex items-center glassmorphism-popup justify-center duration-500 ease-in-out hover:text-white hover:border-white/90 hover:bg-linear-to-r! from-teal-400 to-orange-300 cursor-pointer"
+				className="mt-4 flex hover:scale-105 items-center glassmorphism-popup justify-center duration-500 ease-in-out hover:text-white hover:border-white/50 hover:bg-linear-to-r! from-teal-400 to-orange-300 cursor-pointer"
 				type="submit">Log in</button>
 			</form>
 	
 			<div className='flex items-center justify-center gap-2'>
 				<button
-					className='glassmorphism-popup p-1 cursor-pointer'
-					onClick={handleGoogleLogin}>Google</button>
+					className='glassmorphism-popup hover:scale-3d ease-in p-2 h-11 w-11 flex justify-center items-center cursor-pointer'
+					onClick={handleGoogleLogin}>
+					<img
+						src='https://img.icons8.com/?size=25&id=17949&format=png&color=000000'>
+					</img>
+				</button>
 				<button
-					className='glassmorphism-popup p-1 cursor-pointer'
-					onClick={handle42Login}>42</button>
+					className='glassmorphism-popup hover:scale-3d ease-in p-2 h-11 w-11 flex justify-center items-center cursor-pointer'
+					onClick={handle42Login}>
+					<img
+						src='https://cdn.simpleicons.org/42?viewbox=auto&size=20'>
+					</img>
+				</button>
 				<button
-					className='glassmorphism-popup p-1 cursor-pointer'
-					onClick={gohome}>Go back</button>
+					className='glassmorphism-popup hover:scale-3d ease-in p-2 h-11 w-11 cursor-pointer flex justify-center items-center'
+					onClick={gohome}>
+					<img
+						src="https://img.icons8.com/?size=25&id=83326&format=png&color=000000">
+					</img>
+				</button>
 			</div>
 
 		</div>
