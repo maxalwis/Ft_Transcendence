f0697dcc08b79f3713e1240d7d38bd0ce5b2a861
helasnoussi <ton_email_github@example.com>
2026-08-21 10:59:17 +0200

    fix: local TS and prisma configs

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
+// @ts-nocheck
 import 'dotenv/config';
 import { defineConfig } from '@prisma/config';
-=======
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
->>>>>>> dev
 
 export default defineConfig({
   datasource: {
-<<<<<<< HEAD
-    url: process.env.DATABASE_URL || "postgresql://transcendence:changeme@main_db:5432/transcendence?schema=public",
-=======
-    url: process.env.DATABASE_URL || '',
->>>>>>> dev
+    url: process.env.DATABASE_URL || "postgresql://transcendence:changeme@main_db:5432/transcendence",
   },
 });
