c6b95da56af8adb4ec58060a694c232029a744be
Simon Leroy <sim.leroy@hotmail.fr>
2026-08-23 18:43:07 +0200

    feat(auth): add temporary logout button and forces login to access friends
    -LoginButton now switches between Connexion/Logout based on user state
    -Forces user to login if they try to access Friends without logging

--- front/src/components/BottomBar/Auth/Auth.tsx
+++ front/src/components/BottomBar/Auth/Auth.tsx
@@ -1,7 +1,36 @@
+import { useAuth } from '../../Context/AuthContext.tsx';
+import { useNavigate } from 'react-router-dom';
+
 export default function LoginButton() {
+  const { user, logout } = useAuth();
+	const navigate = useNavigate();
+
   const handleLogin = () => {
     window.location.href = `https://localhost:${import.meta.env.VITE_HTTPS_PORT}/login/`;
   };
+
+  const handleLogout = async () => {
+		try {
+			await fetch(`https://localhost:${import.meta.env.VITE_HTTPS_PORT}/api/auth/logout`, {
+				method: 'POST',
+				credentials: 'include',
+			});
+		} catch {
+			// on ignore une éventuelle erreur réseau, on déconnecte quand même côté client
+		} finally {
+			logout();
+		}
+	};
+
+  if (user) {
+		return (
+			<button
+				className="h-10 w-25 cursor-pointer rounded-full text-white bg-red-600 duration-300 hover:zoom-98"
+				onClick={handleLogout}>
+				Logout
+			</button>
+		);
+	}
 
   return (
     <button
--- front/src/components/Friends/Friends.tsx
+++ front/src/components/Friends/Friends.tsx
@@ -1,6 +1,8 @@
 import FriendsButton from './FriendsButton';
 import FriendsSidebar from './FriendsSidebar';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../components/Context/AuthContext.tsx';
 
 export type FriendAction = 'default' | 'add' | 'remove' | 'request';
 
@@ -19,12 +21,19 @@ export default function Friends() {
   const [friends, setFriends] = useState(['Alice', 'Bob', 'Charlie', 'Max', 'Flav']);
   const [requests, setRequests] = useState(['Diana', 'Evan']);
 
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
   if (!isOpen)
     return (
       <div className="fixed flex flex-col w-50 bottom-2 left-1 z-1000">
         <button
           className="glassmorphism-popup p-2 cursor-pointer hover:bg-sky-900! hover:text-white duration-500 active:scale-70"
           onClick={() => {
+            if (!user) {
+              navigate('/login');
+              return;
+            }
             setAction('default');
             setIsOpen(true);
           }}
