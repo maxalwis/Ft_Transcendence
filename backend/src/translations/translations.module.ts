77c40e17e8099ba2f88c6f860e6c6d856b704647
Simon Leroy <sim.leroy@hotmail.fr>
2026-09-04 15:35:31 +0200

    delete deprecated EventsDetails and unused translation.controller

--- backend/src/translations/translations.module.ts
+++ backend/src/translations/translations.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { LibreTranslateService } from './libretranslate.service';
-import { TranslationsController } from './translations.controller';
 import { TranslationsService } from './translations.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [TranslationsController],
   providers: [TranslationsService, LibreTranslateService],
   exports: [TranslationsService],
 })
